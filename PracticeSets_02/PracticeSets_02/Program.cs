7e88ae56f8158d59f4af310138136f4e015ccb28
darcykekw <darcyclerigo25@gmail.com>
2024-08-21 12:22:46 +0800

    Add files via upload

---
+++ PracticeSets_02/PracticeSets_02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeSets_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 21;
+            for (int i = 2; i < num1; i += 2)
+            { Console.WriteLine("This number is, " + i); }
+        }
+    }
+}
